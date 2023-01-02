d8f6ae21461b3a1974813ebcf49784bd6027aba8
Abhishek Shinde <shindeabhishek113@gmail.com>
2023-01-02 19:08:02 +0530

    changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/index.html'));
+});
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/about.html'));
+});
+
+app.listen(3001);
